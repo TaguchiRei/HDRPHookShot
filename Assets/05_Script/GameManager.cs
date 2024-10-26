333777536a32f0e40f2e21c20dc3367cf2d5b11f
TaguchiRei <ren.len2426@gmail.com>
2024-10-26 18:55:42 +0900

    初コミット 以前のHookShotの続きURL:https://github.com/TaguchiRei/HookShot

---
+++ Assets/05_Script/GameManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public static GameManager Instance = default;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// シーンがロードされたときに呼ばれる
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="mode"></param>
+    void OnSceneLoaded(Scene s, LoadSceneMode mode)
+    {
+
+    }
+}
---
+++ Assets/05_Script/PlayerMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public bool _canMove = false;
+    [SerializeField] float _moveSpeed = 1;
+    public bool _canJump = false;
+    [SerializeField] float _jumpPower = 1;
+
+    void Update()
+    {
+        
+    }
+}
+
+public struct BoolFloatSet
+{
+    
+}
