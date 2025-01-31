09948eee6a8a628f1deed4a86483250042b68686
TaguchiRei <ren.len2426@gmail.com>
2025-01-31 16:49:56 +0900

    ディフェンダーエネミーのプログラムを作成開始

---
+++ Assets/05_Script/EnemyScript/DefenderEnemyController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DefenderEnemyController : EnemyBase
+{
+    bool garding = false;
+    Vector3 movePoint = Vector3.zero;
+    
+    public override void UniqueAction(Vector3 delayedPosition)
+    {
+
+    }
+
+    public override void UniqueInitialization()
+    {
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+    public override void Move(Vector3 position)
+    {
+        base.Move(position);
+        Animator.SetBool("Walking", true);
+    }
+}
